4f14fb76bbce81f49e1131f1ac787b106cf29ed2
freuser <dil8016@mail.ru>
2016-09-13 13:29:17 +0300

    Add worked functions. Add tooltips. Delete spare frame. Small fixes. On button "Run test" temporarly connected function freeze/unfreeze controls as stub.

--- dabanalyzer/gui-qt4.py
+++ dabanalyzer/gui-qt4.py
@@ -10,26 +10,23 @@ class GUIQt(QtGui.QWidget):
     QtGui.QWidget.__init__(self,*args)
     self.setWindowTitle("DAB Analyzer")
     self.resize(800,600)
-    box_lay = QtGui.QVBoxLayout(self)
     self.time = time()
     self.dir_name = None
     self.last_path = ''
     self.error_window = QtGui.QErrorMessage(self)
 
-    self.frame = QtGui.QFrame(self)
-    self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
-    self.frame.setFrameShadow(QtGui.QFrame.Raised)
-    grid_lay = QtGui.QGridLayout(self.frame)
+    grid_lay = QtGui.QGridLayout(self)
 
-    box0 = QtGui.QGroupBox("", self.frame)
-    box0.setSizePolicy(QtGui.QSizePolicy.Preferred,QtGui.QSizePolicy.Fixed)
-    lay0 = QtGui.QHBoxLayout(box0)
-    self.check_thr = QtGui.QCheckBox("Thresh", box0)
+    self.box0 = QtGui.QGroupBox("", self)
+    self.box0.setToolTip("Global threshold for DAB-positive area, from 0 to 100.\nOptimal values are usually located from 35 to 55.\nDefault 40.")
+    self.box0.setSizePolicy(QtGui.QSizePolicy.Preferred,QtGui.QSizePolicy.Fixed)
+    lay0 = QtGui.QHBoxLayout(self.box0)
+    self.check_thr = QtGui.QCheckBox("Thresh", self.box0)
     self.check_thr.setMinimumHeight(30)
     self.spin_thr = QtGui.QSpinBox()
     self.spin_thr.setFixedWidth(60)
     self.spin_thr.setRange(1, 100)
-    self.slider_thr = QtGui.QSlider(QtCore.Qt.Horizontal, box0)
+    self.slider_thr = QtGui.QSlider(QtCore.Qt.Horizontal, self.box0)
     self.slider_thr.setRange(1, 100)
     self.check_thr.stateChanged.connect(self.thresh_proc)
     self.thresh_proc(QtCore.Qt.Unchecked)
@@ -37,18 +34,19 @@ class GUIQt(QtGui.QWidget):
     self.connect(self.slider_thr, QtCore.SIGNAL("valueChanged(int)"), self.spin_thr, QtCore.SLOT("setValue(int)"))
     self.spin_thr.setValue(40)
     lay0.addWidget(self.check_thr)
-    lay0.addWidget(self.slider_thr)
     lay0.addWidget(self.spin_thr)
-    grid_lay.addWidget(box0,0,0,1,8)
+    lay0.addWidget(self.slider_thr)
+    grid_lay.addWidget(self.box0,0,0,1,8)
 
-    box1 = QtGui.QGroupBox("", self.frame)
-    box1.setSizePolicy(QtGui.QSizePolicy.Preferred,QtGui.QSizePolicy.Fixed)
-    lay1 = QtGui.QHBoxLayout(box1)
-    self.check_emp = QtGui.QCheckBox("EMPTY", box1)
+    self.box1 = QtGui.QGroupBox("", self)
+    self.box1.setToolTip("Global threshold for EMPTY area, from 0 to 100.\nDefault disabled.")
+    self.box1.setSizePolicy(QtGui.QSizePolicy.Preferred,QtGui.QSizePolicy.Fixed)
+    lay1 = QtGui.QHBoxLayout(self.box1)
+    self.check_emp = QtGui.QCheckBox("EMPTY", self.box1)
     self.check_emp.setMinimumHeight(30)
-    self.slider_emp = QtGui.QSlider(QtCore.Qt.Horizontal, box1)
+    self.slider_emp = QtGui.QSlider(QtCore.Qt.Horizontal, self.box1)
     self.slider_emp.setRange(1, 100)
-    self.spin_emp = QtGui.QSpinBox(box1)
+    self.spin_emp = QtGui.QSpinBox(self.box1)
     self.spin_emp.setFixedWidth(60)
     self.spin_emp.setRange(1, 100)
     self.check_emp.stateChanged.connect(self.empty_proc)
@@ -56,45 +54,50 @@ class GUIQt(QtGui.QWidget):
     self.connect(self.spin_emp, QtCore.SIGNAL("valueChanged(int)"), self.slider_emp, QtCore.SLOT("setValue(int)"))
     self.connect(self.slider_emp, QtCore.SIGNAL("valueChanged(int)"), self.spin_emp, QtCore.SLOT("setValue(int)"))
     lay1.addWidget(self.check_emp)
-    lay1.addWidget(self.slider_emp)
     lay1.addWidget(self.spin_emp)
-    grid_lay.addWidget(box1,1,0,2,8)
+    lay1.addWidget(self.slider_emp)
+    grid_lay.addWidget(self.box1,1,0,2,8)
 
-    self.check_an = QtGui.QCheckBox("Analyze", self.frame)
+    self.check_an = QtGui.QCheckBox("Analyze", self)
+    self.check_an.setToolTip("Add group analysis after the indvidual image processing.\nThe groups are created using the filename.\nEverything before '_' symbol will be recognized as a group name.\nExample: sample01_10.jpg, sample01_11.jpg will be counted as a single group 'sample01'.\nDefault disabled.")
     grid_lay.addWidget(self.check_an,3,0)
 
-    self.check_sil = QtGui.QCheckBox("Silent", self.frame)
+    self.check_sil = QtGui.QCheckBox("Silent", self)
+    self.check_sil.setToolTip("Supress figure rendering during the analysis,\nonly final results will be saved.\nDefault disabled.")
     grid_lay.addWidget(self.check_sil,4,0)
 
-    box2 = QtGui.QGroupBox("", self.frame)
-    lay2 = QtGui.QVBoxLayout(box2)
-    self.check_mat = QtGui.QCheckBox("Matrix", box2)
-    self.label_json = QtGui.QLabel("", box2)
+    self.box2 = QtGui.QGroupBox("", self)
+    self.box2.setToolTip("Your matrix in a JSON formatted file.\nExperimental option.\nDefault use built-in.")
+    lay2 = QtGui.QVBoxLayout(self.box2)
+    self.check_mat = QtGui.QCheckBox("Matrix", self.box2)
+    self.label_json = QtGui.QLabel("", self.box2)
+    self.check_mat.stateChanged.connect(self.loadJSON)
     lay2.addWidget(self.check_mat)
     lay2.addWidget(self.label_json)
-    grid_lay.addWidget(box2,3,1,2,7)
-    self.check_mat.stateChanged.connect(self.loadJSON)
+    grid_lay.addWidget(self.box2,3,1,2,7)
 
-    box3 = QtGui.QGroupBox("", self.frame)
-    box3.setSizePolicy(QtGui.QSizePolicy.Preferred,QtGui.QSizePolicy.Preferred)
-    lay3 = QtGui.QVBoxLayout(box3)
-    self.open_dir = QtGui.QPushButton('Open directory', box3)
+    self.box3 = QtGui.QGroupBox("", self)
+    self.box3.setSizePolicy(QtGui.QSizePolicy.Preferred,QtGui.QSizePolicy.Preferred)
+    lay3 = QtGui.QVBoxLayout(self.box3)
+    self.open_dir = QtGui.QPushButton('Open directory', self.box3)
     self.connect(self.open_dir, QtCore.SIGNAL('clicked()'), self.showDir)
-    self.open_dir.setToolTip("Open your mind")
-    self.list_dir = QtGui.QListWidget(box3)
+    self.open_dir.setToolTip("Select directory with images for analyzing.")
+    self.list_dir = QtGui.QListWidget(self.box3)
+    self.list_dir.setToolTip("Names of images in current directory.\nSelect any for preview.\nRepeat click on current for deleting preview")
     self.list_dir.currentRowChanged.connect(self.showImage)
     self.list_dir.itemClicked.connect(self.showImage)
-    self.label_dir = QtGui.QLineEdit("",box3)
+    self.label_dir = QtGui.QLineEdit("",self.box3)
+    self.label_dir.setToolTip("Name of current directory.")
     self.label_dir.setReadOnly(True)
     lay3.addWidget(self.open_dir)
     lay3.addWidget(self.label_dir)
     lay3.addWidget(self.list_dir)
-    grid_lay.addWidget(box3,5,0,6,4)
+    grid_lay.addWidget(self.box3,5,0,6,4)
 
-    box4 = QtGui.QGroupBox("", self.frame)
+    box4 = QtGui.QGroupBox("", self)
     lay4 = QtGui.QVBoxLayout(box4)
     self.run_button = QtGui.QPushButton('Run test', box4)
-    self.run_button.clicked.connect(self.getPath)
+    self.run_button.clicked.connect(self.stub)
     self.image_box = QtGui.QGroupBox("", box4)
     image_lay = QtGui.QVBoxLayout(self.image_box)
     self.image_stats = QtGui.QLabel(self.image_box)
@@ -102,7 +105,6 @@ class GUIQt(QtGui.QWidget):
     self.image_stats.setPixmap(self.image_view)
     self.log_output_field = QtGui.QTextEdit(self.image_box)
     self.log_output_field.setReadOnly(True)
-    self.log_output_field.setText('fgn\nb')
     image_lay.addWidget(self.image_stats)
     self.image_stats.setVisible(False)
     image_lay.addWidget(self.log_output_field)
@@ -114,7 +116,18 @@ class GUIQt(QtGui.QWidget):
     grid_lay.setColumnStretch(3,2)
     grid_lay.setColumnStretch(4,3)
     grid_lay.setColumnStretch(5,2)
-    box_lay.addWidget(self.frame)
+    self.stub_flag = False
+
+  def stub(self):
+    if self.stub_flag == True:
+      self.stub_flag = False
+    else:
+      self.stub_flag = True
+    self.freeze(self.stub_flag)
+
+  def freeze(self, state):
+    for i in [self.box0, self.box1, self.box2, self.box3, self.check_an, self.check_sil]:
+      i.setDisabled(state)
 
   def showImage(self):
     if time() - self.time < 0.3:
@@ -176,14 +189,15 @@ class GUIQt(QtGui.QWidget):
     else:
       self.check_mat.setCheckState(QtCore.Qt.Unchecked)
 
-  def showDir(self):
-    dir_name = QtGui.QFileDialog.getExistingDirectory(self, 'Open file', self.last_path)
+  def showDir(self, dir_name = None):
+    if not dir_name:
+      dir_name = QtGui.QFileDialog.getExistingDirectory(self, 'Open directory', self.last_path)
     if dir_name:
       files = [name for name in os.listdir(dir_name) if
               os.path.isfile(os.path.join(dir_name,name)) if
               imghdr.what(os.path.join(dir_name, name))]
       if len(files) == 0:
-        self.error_window.showMessage("You need select directory with images")
+        self.error_window.showMessage("You need select directory with image(s)")
       else:
         self.last_path = self.dir_name = dir_name
         self.label_dir.setText(self.dir_name)
@@ -203,11 +217,19 @@ class GUIQt(QtGui.QWidget):
     else:
       return self.dir_name
 
+  def setPath(self, path):
+    self.last_path = self.dir_name = path
+    self.showDir(path)
+
   def getThresh(self):
     if self.check_thr.checkState() == QtCore.Qt.Unchecked:
       return None
     else:
       return slider_thr.value()
+
+  def setThresh(self, thr):
+    self.check_thr.setCheckState(QtCore.Qt.Checked)
+    self.spin_thr.setValue(thr)
 
   def getEmpty(self):
     if self.check_emp.checkState() == QtCore.Qt.Unchecked:
@@ -215,11 +237,18 @@ class GUIQt(QtGui.QWidget):
     else:
       return slider_emp.value()
 
+  def setEmpty(self, emp):
+    self.check_emp.setCheckState(QtCore.Qt.Checked)
+    self.spin_emp.setValue(thr)
+
   def getAnalyze(self):
     if self.check_an.checkState() == QtCore.Qt.Unchecked:
       return False
     else:
       return True
+
+  def setAnalyze(self):
+    self.check_an.setCheckState(QtCore.Qt.Checked)
 
   def getSilent(self):
     if self.check_sil.checkState() == QtCore.Qt.Unchecked:
@@ -227,12 +256,43 @@ class GUIQt(QtGui.QWidget):
     else:
       return True
 
+  def setSilent(self):
+    self.check_sil.setCheckState(QtCore.Qt.Checked)
+
   def getMatrix(self):
     if self.check_mat.checkState() == QtCore.Qt.Unchecked:
       return None
     else:
       return self.json
 
+  def setMatrix(self, json):
+    self.check_mat.setCheckState(QtCore.Qt.Checked)
+    self.last_path = os.path.dirname(json)
+    self.json = json
+    self.label_json.setText("Selected: %s" %self.json)
+
+  def setAll(self, args):
+    if args.json:
+      self.setMatrix(args.json)
+    if args.path:
+      self.setPath(args.path)
+    if args.thresh:
+      self.setThresh(args.thresh)
+    if args.empty:
+      self.setEmpty(args.empty)
+    if args.analyze:
+      self.setAnalyze()
+    if args.silent:
+      self.setSilent()
+
+  def getAll(self, args):
+    args.path = self.getPath()
+    args.thresh = self.getThresh()
+    args.empty = self.getEmpty()
+    args.analyze = self.getAnalyze()
+    args.silent = self.getSilent()
+    args.json = self.getMatrix()
+    return args
 
 if __name__=="__main__":
     app = QtGui.QApplication(sys.argv)
